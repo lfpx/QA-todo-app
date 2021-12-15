9d6ea692e53c679afa818b9750a2160b1b9086eb
lfpx <luizfpx@gmail.com>
2021-12-15 17:38:48 +0000

    Corrected missing -e parameter on docker run

--- Jenkins/run_container.sh
+++ Jenkins/run_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo '# Running container'
-docker run --rm -d -p 5000:5000 --name myapp -e DATABASE_URI=${DATABASE_URI} CREATE_SCHEMA=${CREATE_SCHEMA} todoapp
+docker run --rm -d -p 5000:5000 --name myapp -e DATABASE_URI=${DATABASE_URI} -e CREATE_SCHEMA=${CREATE_SCHEMA} todoapp
